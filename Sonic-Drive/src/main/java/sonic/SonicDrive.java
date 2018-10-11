6f6842ea9be17898d721c7943cf7b2b0b95fedb2
priyaVora <prvora89@gmail.com>
2018-10-11 10:42:15 -0600

    Fixed/Changes -The manner in which  Executor Services are used.

--- Sonic-Drive/src/main/java/sonic/SonicDrive.java
+++ Sonic-Drive/src/main/java/sonic/SonicDrive.java
@@ -8,12 +8,12 @@ import java.util.concurrent.Semaphore;
 public class SonicDrive {
 
 	final static int CUSTOMERS = 10;
-	final static int MAX_OCCUPANCY = 5;
 	final static int MAX_PARKING_OCCUPANCY = 3;
-	final static int EMPLOYEE = 10;
+	final static int EMPLOYEE = 2;
 
-	private static ExecutorService parkingService;
-	private static ExecutorService employeeService;
+	private static ExecutorService sonicService;
+	private static Semaphore parkingSpots = new Semaphore(MAX_PARKING_OCCUPANCY);
+	private static Semaphore employees = new Semaphore(EMPLOYEE);
 
 	public static Random random = new Random();
 
@@ -22,87 +22,45 @@ public class SonicDrive {
 	}
 
 	public static void generatesCustomers() {
-		Semaphore parkingSpots = new Semaphore(MAX_PARKING_OCCUPANCY);
-		parkingService = Executors.newFixedThreadPool(CUSTOMERS);
-		Semaphore employees = new Semaphore(EMPLOYEE);
-		employeeService = Executors.newFixedThreadPool(EMPLOYEE);
+		sonicService = Executors.newFixedThreadPool(MAX_PARKING_OCCUPANCY);
 
 		for (int i = 0; i < CUSTOMERS; i++) {
 			final int j = i;
 			Customer currentCustomer = new Customer();
-			System.out.println("\n\nCurrent Available parking spots " + parkingSpots.availablePermits());
-
+			//System.out.println("\n\nCurrent Available parking spots " + parkingSpots.availablePermits());
 			System.out.println(
 					"Customer " + currentCustomer.getId() + " wait time starts: " + System.currentTimeMillis());
 			System.out.println("   Diner Session + " + currentCustomer.getId() + " begins");
 
-			int waitForParkingSpot = random.nextInt(10000) + 5000;
+			int waitForParkingSpot = random.nextInt(1000);
 			sleep(waitForParkingSpot);
 			System.out.println("#" + currentCustomer.getId() + " Wait--Parking Spot");
 
-			parkingService.submit(() -> {
+			sonicService.submit(() -> {
 				try {
-					parkingSpots.acquire();
+					if (parkingSpots.availablePermits() != 0) {
+						System.out.println("Customer " + currentCustomer.getId() + " has parking spot.");
+						parkingSpots.acquire();
+					} else { 
+						sleep(2000);
+					}
 				} catch (InterruptedException ex) {
 				}
 				try {
-					System.out.println("Customer " + currentCustomer.getId() + " has parking spot.");
-					int waitForEmployeeToTakeOrder = random.nextInt(10);
+					int waitForEmployeeToTakeOrder = random.nextInt(1000);
 					try {
 						Thread.sleep(waitForEmployeeToTakeOrder);
 						System.out.println("#" + currentCustomer.getId() + " wait for employee.("
 								+ waitForEmployeeToTakeOrder + ")");
-
-						employeeService.submit(() -> {
-							try {
-								employees.acquire();
-								System.out.println("#" + currentCustomer.getId() + " employee is available for order.");
-							} catch (InterruptedException ex) {
-
-							}
-							try {
-								currentCustomer.run();
-								System.out.println(
-										"#" + currentCustomer.getId() + " employee processes the customer's payment.");
-
-								////////////////////////////////////////////////////////
-								int preparingOrder = random.nextInt(5000);
-								sleep(preparingOrder);
-								System.out.println(
-										"#" + currentCustomer.getId() + "'s order prepared (" + preparingOrder + ")");
-								////////////////////////////
-								/**
-								 * Employee must be available to deliver food
-								 */
-
-								employeeService.submit(() -> {
-									try {
-										employees.acquire();
-									} catch (InterruptedException ex) {
-									}
-									try {
-										System.out.println("Employee deliver's food: " + currentCustomer.getId());
-									} finally {
-										employees.release();
-									}
-								});
-								///////////////////////////
-								int customerMealTime = random.nextInt(3000);
-								sleep(customerMealTime);
-								System.out.println(
-										"#" + currentCustomer.getId() + " ate meal..." + "(" + customerMealTime + ")");
-
-								System.out
-										.println("Customer #" + currentCustomer.getId() + " vacates the parking lot.");
-							} finally {
-								employees.release();
-							}
-						});
+						employees.acquire();
 
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
+					/// ---
+
+					employeeService(currentCustomer);
 
 				} finally {
 					parkingSpots.release();
@@ -112,6 +70,78 @@ public class SonicDrive {
 
 		}
 
+	}
+
+	private static void employeeService(Customer currentCustomer) {
+		checkEmployeeAvailability(currentCustomer);
+		try {
+			processPayment(currentCustomer);
+			////////////////////////////////////////////////////////
+			prepareOrder(currentCustomer);
+			////////////////////////////
+			/**
+			 * Employee must be available to deliver food
+			 */
+			foodDelivery(currentCustomer);
+			///////////////////////////
+			mealTime(currentCustomer);
+			System.out.println("Customer #" + currentCustomer.getId() + " vacates the parking lot.");
+		} finally {
+			employees.release();
+		}
+
+	}
+
+	private static void mealTime(Customer currentCustomer) {
+		int customerMealTime = random.nextInt(3000);
+		sleep(customerMealTime);
+		System.out.println("#" + currentCustomer.getId() + " ate meal..." + "(" + customerMealTime + ")");
+	}
+
+	private static void foodDelivery(Customer currentCustomer) {
+		try {
+			if (employees.availablePermits() != 0) {
+				employees.acquire();
+			} else {
+				sleep(2000);
+			}
+			System.out.println("Employee deliver's food: " + currentCustomer.getId());
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			employees.release();
+		}
+
+	}
+
+	public static void checkEmployeeAvailability(Customer currentCustomer) {
+		try {
+			if (employees.availablePermits() != 0) {
+				System.out.println("#" + currentCustomer.getId() + " employee is available for order.");
+				employees.acquire();
+			} else {
+				sleep(random.nextInt(2000));
+			}
+
+		} catch (InterruptedException ex) {
+
+		}
+	}
+
+	public static void prepareOrder(Customer currentCustomer) {
+		int preparingOrder = random.nextInt(5000);
+		sleep(preparingOrder);
+		System.out.println("#" + currentCustomer.getId() + "'s order prepared (" + preparingOrder + ")");
+	}
+
+	public static void processPayment(Customer currentCustomer) {
+		currentCustomer.run();
+		if (employees.availablePermits() != 0) {
+			System.out.println("#" + currentCustomer.getId() + " employee processes the customer's payment.");
+		} else {
+			sleep(random.nextInt(2000));
+		}
 	}
 
 	private static void sleep(long t) {
